aa2afebb36bc343ea4d72fbf4c6a6f35e0f94654
Warren <warren.buch16@gmail.com>
2022-07-18 17:08:34 +0200

    I did the js so that the button which are pushed are printed on the screen. But it doesnt do arithmetic for the moment thats the next step :D

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+let screenValue = ""
+
+function getValue(item)//get the value of a button and print it on the calculator screen
+{ 
+    const val = document.getElementById(item).value
+    const text = document.getElementById("numbers")
+    if(val === "reset")
+    {
+        text.innerHTML = ""
+        screenValue = ""
+        return
+    }
+    if(val === 'del')
+    {
+        screenValue = screenValue.slice(0, -1)
+        text.innerHTML = screenValue
+        return
+    }
+    text.innerHTML = screenValue + val
+    screenValue += val
+}
+
+function calculate() // calculate the operation on the screen
+{
+    const text = document.getElementById("numbers")
+    const result = parseInt(screenValue)
+    console.log(screenValue)
+    console.log(result)
+    text.innerHTML = result
+}
